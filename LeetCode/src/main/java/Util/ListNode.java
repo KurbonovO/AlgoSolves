a3741a5ced6bb7ed9305938628c13ac89c92d469
Beck Kurbonov <beckkurbonov@gmail.com>
2023-08-24 20:16:35 -0400

    August 24

---
+++ LeetCode/src/main/java/Util/ListNode.java
@@ -0,0 +1,19 @@
+package Util;
+
+public class ListNode {
+
+    public int val = 0;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
